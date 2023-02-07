ec2568e61405587f534f175c588a3a026abe2560
Trvagner <116954836+Trvagner@users.noreply.github.com>
2023-02-07 06:30:36 -0300

    Atualização projeto v.2
    Adicionando Menu

--- src/paginas/Paginainicial/index.jsx
+++ src/paginas/Paginainicial/index.jsx
@@ -1,7 +1,22 @@
+import Banner from './banner.png'
 import Cabecalho from "../../componentes/Cabecalho";
+import Menu from "../../componentes/Menu";
+import styles from './Paginainicial.module.scss';
 
 export default function PaginaInicial () {
     return (
-        <Cabecalho/>
-    )
+        <>
+            <Cabecalho/>
+            <main>
+                <section className={styles.principal}>
+                    <Menu/>
+                    <div className={styles.principal__imagem}>
+                        <h1>A galeria mais completa do espaço</h1>
+                        <img src={Banner} alt="A imagem da terra vista do espaço"/>
+                    </div>
+                </section>
+            </main>
+        </>
+        
+   )
 }
